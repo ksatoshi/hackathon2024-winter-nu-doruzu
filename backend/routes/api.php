7740e1b85e2c4f89fe1d8bcc2e9471345c7cefa0
ksatoshi <ksatoshi@mail.ksatoshi.dev>
2024-02-28 17:27:41 +0900

    企業情報一覧に座標情報を追加して返す物を仮置き

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -17,3 +17,56 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/release',function (){
+    // TODO:さすがにもう少し整える必要がある
+
+    $APIKEY = env('PRTIMES_APIKEY');
+
+    $companylist_base_url = 'https://hackathon.stg-prtimes.net/api/companies';
+
+    $client = new GuzzleHttp\Client();
+    $res = $client->request('GET',$companylist_base_url,[
+        'headers' => array(
+            'Accept' => 'application/json',
+            'Authorization' => 'Bearer '. $APIKEY
+        )
+    ]);
+
+    $companylist = json_decode($res->getBody(),true);
+
+    $result = array();
+
+    foreach ($companylist as $company){
+        $address = $company['address'];
+
+        $coordinate_reference_url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/'.urlencode($address).'.json';
+
+        $mapbox_api_key = env('MAPBOX_APIKEY');
+
+
+        $res = $client->request('GET',$coordinate_reference_url,[
+            'query'=> array(
+                'access_token'=>$mapbox_api_key
+            )
+        ]);
+
+        $address_reference_origin = json_decode($res->getBody(),true);
+
+        /*dd();
+        dd($address_reference_origin);*/
+
+        // とりあえず一致度の高い先頭のみを使用する
+        try{
+            $coordinate = $address_reference_origin['features'][0]['geometry']['coordinates'];
+        }catch (Exception $exception){
+            continue;
+        }
+
+        $company['coordinate'] = $coordinate;
+
+        $result[] = $company;
+    }
+
+    return $result;
+});
