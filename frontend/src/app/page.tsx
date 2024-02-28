28468c5b322b33a1c996baa2736bf2a41beea9bf
Kyo Terada <sitiang120@gmail.com>
2024-02-28 17:10:19 +0900

    検索窓の追加

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,11 @@
 import SimpleMap from "@/components/Map";
+import SearchInput from "@/components/SearchInput";
 
 export default function Home() {
   return (
     <div className="h-screen w-screen flex items-center justify-center">
       <div className="w-full h-full">
+        <SearchInput />
         <SimpleMap />
       </div>
     </div>
