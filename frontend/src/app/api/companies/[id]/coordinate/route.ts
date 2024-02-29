c0bf581e21b4616d32c8d1857da2cff5e9ebd9b3
Eakudompong Chanoknan <ceguantz96@outlook.com>
2024-02-29 19:04:26 +0900

    企業リスト route 追加

--- frontend/src/app/api/companies/[id]/coordinate/route.ts
+++ frontend/src/app/api/companies/[id]/coordinate/route.ts
@@ -1,32 +1,41 @@
-import {NextRequest, NextResponse} from "next/server";
+import { NextRequest, NextResponse } from 'next/server'
 
-export async function GET(req:NextRequest,{params}:{params:{id:string}}){
-    const PRTIMES_APIKEY = process.env.PRTIMES_APIKEY;
-    const MAPBOX_APIKEY = process.env.MAPBOX_APIKEY;
+export async function GET(
+  req: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  const PRTIMES_APIKEY = process.env.PRTIMES_APIKEY
 
-    const company_id = params.id;
-    const companydetail_url = 'https://hackathon.stg-prtimes.net/api/companies/'+company_id;
+  const MAPBOX_APIKEY = process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN
 
-    let prtimes_req = await fetch(companydetail_url,{
-        method:'GET',
-        headers:{
-            'Accept':'application/json',
-            'Authorization':'Bearer '+PRTIMES_APIKEY
-        }
-    })
+  const company_id = params.id
+  const companydetail_url =
+    'https://hackathon.stg-prtimes.net/api/companies/' + company_id
 
-    let companydetail_origin = await prtimes_req.json();
-    let company_address = companydetail_origin['address'];
+  let prtimes_req = await fetch(companydetail_url, {
+    method: 'GET',
+    headers: {
+      Accept: 'application/json',
+      Authorization: 'Bearer ' + PRTIMES_APIKEY
+    }
+  })
 
-    const geocoding_url = "https://api.mapbox.com/geocoding/v5/mapbox.places/"+encodeURIComponent(company_address)+".json?access_token="+MAPBOX_APIKEY;
+  let companydetail_origin = await prtimes_req.json()
+  let company_address = companydetail_origin['address']
 
-    let mapbox_req = await fetch(geocoding_url);
-    let geocoding_origin = await mapbox_req.json();
+  const geocoding_url =
+    'https://api.mapbox.com/geocoding/v5/mapbox.places/' +
+    encodeURIComponent(company_address) +
+    '.json?access_token=' +
+    MAPBOX_APIKEY
 
-    let coordinate = geocoding_origin['features'][0]['geometry']['coordinates'];
-    console.log(coordinate)
+  let mapbox_req = await fetch(geocoding_url)
+  let geocoding_origin = await mapbox_req.json()
 
-    let result = {'coordinate':[coordinate[0],coordinate[1]]}
+  let coordinate = geocoding_origin['features'][0]['geometry']['coordinates']
+  console.log(coordinate)
 
-    return NextResponse.json(result);
+  let result = { coordinate: [coordinate[0], coordinate[1]] }
+
+  return NextResponse.json(result)
 }
