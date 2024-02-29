93daef57f4d91540444e93b1453f93d7bed34ec9
Eakudompong Chanoknan <ceguantz96@outlook.com>
2024-02-29 22:44:06 +0900

    Listview スタイル修正

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -4,10 +4,8 @@ import Listview from '@/components/Listview'
 export default function Home() {
   return (
     <div className="h-screen w-screen flex  items-center justify-between">
-
       <SimpleMap />
       <Listview />
-
     </div>
   )
 }
